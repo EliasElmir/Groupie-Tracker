a8fca9199fc5e029ee3caf337cd13a1fe019991a
EliasElmir <150239298+EliasElmir@users.noreply.github.com>
2024-03-14 14:30:30 +0100

    viewMain.go

---
+++ view/viewMain.go
@@ -0,0 +1,20 @@
+package view
+
+import (
+	"groupietracker/model"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+var (
+	myApp model.AppData
+)
+
+func Begin() {
+	myApp.App = app.New()
+	myApp.Window = myApp.App.NewWindow("Groupie Tracker.")
+	MainPage()
+	myApp.Window.Resize(fyne.NewSize(225, 875))
+	myApp.Window.ShowAndRun()
+}
