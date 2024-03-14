a76bd0811d040c92050a0503df041b3d960a5d99
EliasElmir <150239298+EliasElmir@users.noreply.github.com>
2024-03-14 12:55:23 +0100

    base.go

--- view/base.go
+++ view/base.go
@@ -8,8 +8,6 @@ import (
 	"strconv"
 	"time"
 
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
@@ -51,3 +49,71 @@ func MainPage() {
 			},
 		),
 	)
+	switch filter {
+	case 1:
+		grid.RemoveAll()
+		for id := 1; id < 53; id++ {
+			grid.Add(ButtonImg(id))
+		}
+	case 2:
+		grid.RemoveAll()
+		artists := make([]*model.Artist, 52)
+		for i := 1; i <= 52; i++ {
+			artists[i-1] = testmodel.GetArtistsID(i)
+		}
+		sort.Slice(artists, func(i, j int) bool {
+			return artists[i].CreationDate < artists[j].CreationDate
+		})
+		for i, artist := range artists {
+			fmt.Println(strconv.Itoa(i+1)+".", artist.Name, "(", artist.CreationDate, ")")
+			grid.Add(ButtonImg(int(artist.Id)))
+		}
+	case 3:
+		grid.RemoveAll()
+		artists := make([]*model.Artist, 52)
+		for i := 1; i <= 52; i++ {
+			artists[i-1] = testmodel.GetArtistsID(i)
+		}
+		sort.Slice(artists, func(i, j int) bool {
+			return artists[i].CreationDate > artists[j].CreationDate
+		})
+		for i, artist := range artists {
+			fmt.Println(strconv.Itoa(i+1)+".", artist.Name, "(", artist.CreationDate, ")")
+			grid.Add(ButtonImg(int(artist.Id)))
+		}
+	case 4:
+		grid.RemoveAll()
+		artists := make([]*model.Artist, 52)
+		for i := 1; i <= 52; i++ {
+			artists[i-1] = testmodel.GetArtistsID(i)
+		}
+		sort.Slice(artists, func(i, j int) bool {
+			time1, _ := time.Parse("02-01-2006", artists[i].FirstAlbum)
+			time2, _ := time.Parse("02-01-2006", artists[j].FirstAlbum)
+			return time1.Year() < time2.Year()
+		})
+		for i, artist := range artists {
+			fmt.Println(strconv.Itoa(i+1)+".", artist.Name, "(", artist.FirstAlbum, ")")
+			grid.Add(ButtonImg(int(artist.Id)))
+		}
+	case 5:
+		grid.RemoveAll()
+		artists := make([]*model.Artist, 52)
+		for i := 1; i <= 52; i++ {
+			artists[i-1] = testmodel.GetArtistsID(i)
+		}
+		sort.Slice(artists, func(i, j int) bool {
+			time1, _ := time.Parse("02-01-2006", artists[i].FirstAlbum)
+			time2, _ := time.Parse("02-01-2006", artists[j].FirstAlbum)
+			return time1.Year() > time2.Year()
+		})
+		for i, artist := range artists {
+			fmt.Println(strconv.Itoa(i+1)+".", artist.Name, "(", artist.FirstAlbum, ")")
+			grid.Add(ButtonImg(int(artist.Id)))
+		}
+	}
+	grid2 := container.NewVScroll(grid)
+	grid2.Refresh()
+	full := container.NewBorder(toolBar, nil, nil, nil, grid2)
+	myApp.Window.SetContent(full)
+}
