6e71da36f168f9ab81d031c23abef87fbac41b31
EliasElmir <150239298+EliasElmir@users.noreply.github.com>
2024-03-28 16:02:39 +0100

    image sur la page d'accueil

--- view/menu.go
+++ view/menu.go
@@ -2,6 +2,7 @@ package view
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
@@ -11,9 +12,15 @@ func home() {
 	testButton := widget.NewButton("Enter in Groupie Tracker", func() {
 		MainPage()
 	})
-	content := container.NewVBox(testButton)
+	imageURL := "https://img.freepik.com/vecteurs-libre/illustration-vectorielle-micro-haut-parleurs-noirs-logo-promotionnel-vintage-pour-concert-festival-musique_74855-10591.jpg"
+	imageContent, _ := fyne.LoadResourceFromURLString(imageURL)
+	image := canvas.NewImageFromResource(imageContent)
+	image.FillMode = canvas.ImageFillOriginal
+	image.SetMinSize(fyne.NewSize(482, 482))
+	content := container.NewVBox(image, testButton)
 	centeredContent := container.NewCenter(content)
 	myApp.Window.SetContent(centeredContent)
-	myApp.Window.Resize(fyne.NewSize(225, 875))
+	myApp.Window.Resize(fyne.NewSize(100, 100))
+	myApp.Window.CenterOnScreen()
 	myApp.Window.Show()
 }
