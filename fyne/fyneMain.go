a0176436e2259c917852461f1be5f5375a2c5839
EliasElmir <150239298+EliasElmir@users.noreply.github.com>
2024-04-02 19:47:44 +0200

    mise a jour

--- fyne/fyneMain.go
+++ fyne/fyneMain.go
@@ -12,7 +12,7 @@ var (
 func Begin() {
 	// Initializes the Fyne application, creates a new window, starts the home page, sets the window size, and displays the window.
 	myApp.App = app.New()
-	myApp.Window = myApp.App.NewWindow("Groupie Tracker.")
+	myApp.Window = myApp.App.NewWindow("FestivaSync")
 	// MainPage()
 	home()
 	myApp.Window.Resize(fyne.NewSize(225, 875))
